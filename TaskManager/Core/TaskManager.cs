a588a843e6b0ae05531d9d5c5bea819f6450f890
jmaynard-n <andreyeva1902@yandex.ru>
2019-12-25 00:24:39 +0300

    complete

---
+++ TaskManager/Core/TaskManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TaskManagerRefact.Core
+{
+    public class TaskManager
+    {
+        private readonly ConcurrentQueue<Action> _actionsQueue = new ConcurrentQueue<Action>();
+        private readonly AutoResetEvent _waitHandler = new AutoResetEvent(false);
+        private readonly Thread _mainThread;
+
+        private List<WorkThread> _idleThreads;
+        private List<WorkThread> _workingThreads;
+
+        private object _locker = new object();
+
+        // Объявляем делегат
+        public delegate void TaskManagerWorkDoneHandler();
+        // Событие, возникающее при выводе денег
+        public event TaskManagerWorkDoneHandler TaskManagerWorkDone;
+
+        public TaskManager(int threadsNum)
+        {
+            _idleThreads = new List<WorkThread>(threadsNum);
+            _workingThreads = new List<WorkThread>(threadsNum);
+
+            for (int i = 0; i < threadsNum; i++)
+            {
+                var wt = new WorkThread();
+                wt.WorkDone += SubThreadWorkDoneHandler;
+                _idleThreads.Add(wt);
+            }
+        }
+
+        private void SubThreadWorkDoneHandler(WorkThread workThread)
+        {
+            if (_actionsQueue.TryDequeue(out Action result))
+            {
+                workThread.Do(result);
+                return;
+            }
+
+            lock (_locker)
+            {
+                _workingThreads.Remove(workThread);
+                _idleThreads.Add(workThread);
+                if(_actionsQueue.IsEmpty && _workingThreads.Count == 0)
+                    TaskManagerWorkDone?.Invoke();
+            }
+        }
+
+
+        public void AddTask(Action action)
+        {
+            lock (_locker)
+            {
+                if (_idleThreads.Count > 0)
+                {
+                    WorkThread wt = _idleThreads[_idleThreads.Count - 1];
+                    _idleThreads.Remove(wt);
+                    _workingThreads.Add(wt);
+                    wt.Do(action);
+                }
+                else
+                {
+                    _actionsQueue.Enqueue(action);
+                }
+
+            }
+        }
+    }
+}
---
+++ TaskManager/Core/WorkThread.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TaskManagerRefact.Core
+{
+    public class WorkThread
+    {
+        private readonly AutoResetEvent _waitHandler = new AutoResetEvent(false);
+        private readonly Thread _thread;
+        private bool _needToTerminate = false;
+        private WorkThreadState _state;
+
+        // Объявляем делегат
+        public delegate void WorkDoneHandler(WorkThread workThread);
+        // Событие, возникающее при выводе денег
+        public event WorkDoneHandler WorkDone;
+
+        private Action _action;
+        public WorkThread()
+        {
+            _thread = new Thread(WorkLoop);
+            _thread.IsBackground = false;
+            _state = WorkThreadState.Idle;
+            _thread.Start();
+        }
+
+        public void Do(Action action)
+        {
+            if (_state != WorkThreadState.Idle)
+                throw new Exception($"Thread in state {_state}");
+            _state = WorkThreadState.Working;
+            _action = action;
+            _waitHandler.Set();
+        }
+
+        public void Terminate()
+        {
+            if (_state != WorkThreadState.Idle)
+                throw new Exception($"Thread in state {_state}");
+            _needToTerminate = true;
+            _waitHandler.Set();
+        }
+
+        public void WorkLoop()
+        {
+            do
+            {
+                _waitHandler.WaitOne();
+                if (_needToTerminate)
+                {
+                    _state = WorkThreadState.Terminated;
+                    break;
+                }
+                    
+
+                _action.Invoke();
+
+                _waitHandler.Reset();
+                _state = WorkThreadState.Idle;
+                WorkDone?.Invoke(this);
+
+            }while(!_needToTerminate);
+
+        }
+
+    }
+
+    public enum WorkThreadState
+    {
+        Working = 1,
+        Idle = 2,
+        Terminated = 3
+    }
+}
